8ee9208cedab43965f745f2db8342c0c671f515b
Bruno Schubert Medeiros <brunoschubertm@gmail.com>
2020-10-05 14:07:55 -0300

    Created Basic Activities.

--- app/src/main/java/com/vizlab/litoAr/MainActivity.java
+++ app/src/main/java/com/vizlab/litoAr/MainActivity.java
@@ -2,7 +2,12 @@ package com.vizlab.litoAr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.vizlab.litoAr.DetectSample.DetectSampleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button launch_detect = (Button) findViewById(R.id.btn_launch_detection);
+        launch_detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), DetectSampleActivity.class);
+                view.getContext().startActivity(intent);}
+        });
     }
 }
