4438b6de0c3270054149f67bb59d3ca16b71baba
Bruno Schubert Medeiros <brunoschubertm@gmail.com>
2020-10-19 22:36:08 -0300

    Added Matching to the Detected Image to its Source Folder. Created File Browser Screen.

--- app/src/main/java/com/vizlab/litoAr/DetectSample/ui/BrowseFilesFragment.java
+++ app/src/main/java/com/vizlab/litoAr/DetectSample/ui/BrowseFilesFragment.java
@@ -7,8 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.vizlab.litoAr.R;
+
+import java.io.File;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +29,22 @@ public class BrowseFilesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_browse_files, container, false);
 
+        TextView sampleName = view.findViewById(R.id.toolbar_title);
+        TextView sampleDescription = view.findViewById(R.id.description_title);
+        TextView fileList = view.findViewById(R.id.file_list);
+
+        sampleName.setText(getArguments().getString("name"));
+        sampleDescription.setText(getArguments().getString("description"));
+
+        File sampleFolder = new File(getArguments().getString("path"));
+        File[] subFolders = sampleFolder.listFiles();
+
+        String folders = "Files encountered: ";
+        for (int i = 0; i < subFolders.length; ++i) {
+            folders = folders + subFolders[i].getName() + "\n";
+        }
+
+        fileList.setText(folders);
 
         return view;
     }
